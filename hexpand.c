48da175fb6d29c213e766489bd8841827738ad54
Anthony Weems <amlweems@gmail.com>
2021-05-04 08:14:10 -0500

    switch to EVP_MD functions for openssl-1.1.x

--- hexpand.c
+++ hexpand.c
@@ -19,7 +19,8 @@ unsigned int strntoul(const char* str, int length, int base) {
 }
 
 char* sha1_extend(EVP_MD_CTX *mdctx, char* signature, int length) {
-	int length_modulo = mdctx->digest->block_size;
+	const EVP_MD *md = EVP_MD_CTX_md(mdctx);
+	int length_modulo = EVP_MD_block_size(md);
 	int length_bytes = length_modulo/8;
 	int trunc_length = length%length_modulo;
 	int padding = ((trunc_length) < (length_modulo-length_bytes))
@@ -29,8 +30,8 @@ char* sha1_extend(EVP_MD_CTX *mdctx, char* signature, int length) {
 	memset(data, 'A', length+padding+length_bytes);
 	EVP_DigestUpdate(mdctx, data, length+padding+length_bytes);
 
-	unsigned char* h_data = (unsigned char *)((SHA512_CTX *)mdctx->md_data)->h;
-	int h_data_size = (mdctx->digest->md_size);
+	unsigned char* h_data = (unsigned char *)((SHA512_CTX *)EVP_MD_CTX_md_data(mdctx))->h;
+	int h_data_size = EVP_MD_size(md);
 	int sha_switch = length_modulo/16;
 	int i = 0, j = 0;
 	while (i < h_data_size) {
@@ -44,15 +45,16 @@ char* sha1_extend(EVP_MD_CTX *mdctx, char* signature, int length) {
 	output[0] = '8';
 	output[1] = '0';
 	for (i = 1; i < 2*(padding+length_bytes); i++) output[i] = '0';
-	if (length_modulo == 128) sprintf(output+2*padding, "%032" PRIx32 , htole64(8*length));
-	else sprintf(output+2*padding, "%016" PRIx32 , htole64(8*length));
+	if (length_modulo == 128) sprintf(output+2*padding, "%032lu", htole64(8*length));
+	else sprintf(output+2*padding, "%016lu", htole64(8*length));
 	output[2*(padding+length_bytes)] = 0;
 
 	return output;
 }
 
 char* md5_extend(EVP_MD_CTX *mdctx, char* signature, int length) {
-	int length_modulo = mdctx->digest->block_size;
+	const EVP_MD *md = EVP_MD_CTX_md(mdctx);
+	int length_modulo = EVP_MD_block_size(md);
 	int length_bytes = length_modulo/8;
 	int trunc_length = length&0x3f;
 	int padding = ((trunc_length) < (length_modulo-length_bytes))
@@ -61,10 +63,10 @@ char* md5_extend(EVP_MD_CTX *mdctx, char* signature, int length) {
 	unsigned char data[length+padding+length_bytes];
 	memset(data, 'A', length+padding+length_bytes);
 	EVP_DigestUpdate(mdctx, data, length+padding+length_bytes);
-	((MD5_CTX *)mdctx->md_data)->A = htonl(strntoul(signature, 8, 16));
-	((MD5_CTX *)mdctx->md_data)->B = htonl(strntoul(signature+8, 8, 16));
-	((MD5_CTX *)mdctx->md_data)->C = htonl(strntoul(signature+16, 8, 16));
-	((MD5_CTX *)mdctx->md_data)->D = htonl(strntoul(signature+24, 8, 16));
+	((MD5_CTX *)EVP_MD_CTX_md_data(mdctx))->A = htonl(strntoul(signature, 8, 16));
+	((MD5_CTX *)EVP_MD_CTX_md_data(mdctx))->B = htonl(strntoul(signature+8, 8, 16));
+	((MD5_CTX *)EVP_MD_CTX_md_data(mdctx))->C = htonl(strntoul(signature+16, 8, 16));
+	((MD5_CTX *)EVP_MD_CTX_md_data(mdctx))->D = htonl(strntoul(signature+24, 8, 16));
 
 	int i;
 	char* output = malloc((2*(padding+length_bytes)+1)*sizeof(char));
